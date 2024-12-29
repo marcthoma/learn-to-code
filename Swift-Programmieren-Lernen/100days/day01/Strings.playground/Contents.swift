f50a406588e6e6378211fff63849468a1306c632
Marc Oliver Thoma <marc.thoma@mth-training.de>
2024-12-29 14:56:59 +0100

    Swift lernen Tag 01

---
+++ Swift-Programmieren-Lernen/100days/day01/Strings.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let actor = "Denzel Washington"
+let movie = "\"The Shawshank Redemption\""
+let result = "⭐️ You win! ⭐️"
+
+
+print("\(actor) played \(movie)")
+
+let movie1 = """
+A day in 
+the life of an
+Apple engineer
+"""
+print (movie1)
+
+let filename = "movie.jpg"
+
+print (actor.count)
+
+let nameLength = actor.count
+print (nameLength)
+
+print (result.uppercased())
+
+print (movie1.hasPrefix("A day"))
+
+print (filename.hasSuffix(".txt"))
+
---
+++ Swift-Programmieren-Lernen/100days/day01/Var_and_Let.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print (playerName)
+
+playerName = "Dany"
+print (playerName)
+
+playerName = "Sam"
+print(playerName)
+
+playerName = "Keeley"
+
+let managerName = "Marc Oliver Thoma"
+print(managerName)
+
+
