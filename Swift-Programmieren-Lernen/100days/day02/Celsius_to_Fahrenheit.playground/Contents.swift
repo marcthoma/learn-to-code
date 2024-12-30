1cbafa91f64a0c239708d800bcbedac3cecb2371
Marc Oliver Thoma <marc.thoma@mth-training.de>
2024-12-30 11:25:54 +0100

    Weitere Übungen

---
+++ Swift-Programmieren-Lernen/100days/day02/Celsius_to_Fahrenheit.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+let temp_C = 23.0
+let temp_F = temp_C * 9 / 5 + 32
+
+print ("Die Temperatur in Celsisu beträgt \(temp_C)° und die Temperatarur in Fahrenheit beträgt \(temp_F)° .")
+
