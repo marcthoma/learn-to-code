1b04b2e7f25af283771da09564832926f0ade135
Marc Oliver Thoma <marc.thoma@mth-training.de>
2025-01-07 18:52:04 +0100

    Ende von Tag 6

--- Swift-Programmieren-Lernen/100days/day06/day06.playground/Pages/Skip Loops.xcplaygroundpage/Contents.swift
+++ Swift-Programmieren-Lernen/100days/day06/day06.playground/Pages/Skip Loops.xcplaygroundpage/Contents.swift
@@ -1,4 +1,29 @@
 import Cocoa
 var greeting = "Hello, playground"
 
+let filenames = ["me.jpg", "work.txt", "sophie.jpg"]
+
+for filename in filenames {
+    if filename.hasSuffix (".jpg") == false {
+    //if !filename.hasSuffix (".jpg"){   --> Das funktioniert genauso!
+        continue
+    }
+    print (filename)
+}
+
+let number1 = 4
+let number2 = 14
+
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+    }
+    if multiples.count == 10 {
+        break
+    }
+}
+print (multiples)
+
 
