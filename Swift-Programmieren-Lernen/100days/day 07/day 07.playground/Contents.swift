91ba97f3c62d00ffbc7d1f965e4f3014da3b2b9e
Marc Oliver Thoma <marc.thoma@mth-training.de>
2025-01-08 23:51:44 +0100

    Die ersten Schritte von Tag 7

---
+++ Swift-Programmieren-Lernen/100days/day 07/day 07.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Cocoa
+
+func showWelcome()  {
+    print ("Hier steht etwas")
+    print ("Hier steht noch viel mehr")
+    print ("Was hier steht ist sogar noch wichtig")
+    print ("Am Ende soll das ohnehin alles in einer Funktion stehen")
+}
+
+showWelcome()
+
+let number = 129
+
+if number.isMultiple(of: 2) {
+    print("Even")
+} else {
+    print("Odd")
+}
+
+func printTimeTables(number: Int, end: Int) {
+    for i in 1...end {
+        print ("\(number) * \(i) is \(number * i)")
+    }
+}
+
+printTimeTables(number: 5, end: 10)
+ 
+
